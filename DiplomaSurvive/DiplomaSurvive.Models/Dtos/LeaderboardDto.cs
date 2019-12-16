81ac0f43ebafcdba898b6afcf1b17207ac3f3c8e
Vlada <vlada.lipska@plarium.com>
2019-12-16 21:33:48 +0200

    DS-3: added models library

---
+++ DiplomaSurvive/DiplomaSurvive.Models/Dtos/LeaderboardDto.cs
@@ -0,0 +1,18 @@
+﻿namespace DiplomaSurvive.Models
+{
+    public class LeaderboardDto
+    {
+        public int Place { get; set; }
+        
+        public LeaderboardPlayer[] Players { get; set; }
+
+        public class LeaderboardPlayer
+        {
+            public int Place { get; set; }
+            
+            public string Name { get; set; }
+            
+            public int Score { get; set; }
+        }
+    }
+}
---
+++ DiplomaSurvive/DiplomaSurvive.Models/Enums/SecurityCode.cs
@@ -0,0 +1,13 @@
+﻿namespace DiplomaSurvive.Models
+{
+    /// <summary>
+    /// Security errors enum.
+    /// </summary>
+    public enum SecurityErrorCode
+    {
+        /// <summary>
+        /// The user is unrecognized.
+        /// </summary>
+        UnrecognizedUser = 1
+    }
+}
